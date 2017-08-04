99703ace85693bae80134e02a792d1969e8f7fe5
Cory Forsyth and Jonathan Jackson <dev+bantic+rondale-sc@hashrocket.com>
2017-08-04 15:00:30 -0400

    Create macro-benchmark executable
    This is in service of the Ember Macro Benchmark Quest Issue:
    https://github.com/krisselden/ember-macro-benchmark/issues/27
    
    The first command is designed to create the skeleton for new
    configuration folders for the perProjectConfig section in the new
    consumed-application design.

---
+++ bin/macro-benchmark.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+let fs = require('fs-extra');
+let path = require('path');
+let yargs = require('yargs');
+let { stripIndent } = require('common-tags');
+
+require('yargs')
+  .usage('$0 <cmd> [args]')
+  .command('add-config [name]', 'Add configuration folder to macro-benchmark repo', {
+    name: {
+      describe: 'Name of configuration folder. e.g. v3.10.1 or pre-hydration-glimmer.'
+    }
+  }, addConfig)
+  .help()
+  .argv
+
+function addConfig(options) {
+  let { name } = options;
+  let cwd = process.cwd();
+
+  if (fs.existsSync(path.join(cwd, 'config'))) {
+
+    let archivePath = path.join(cwd, 'config', name, 'archives', '.gitkeep');
+    fs.outputFileSync(archivePath, '');
+
+    let perProjectConfigFile = path.join(cwd, 'config', name, 'index.js');
+    let perProjectConfig = stripIndent`
+      module.exports = {
+        har: false,
+        name: ${name},
+        fingerprinted: false
+      }` + "\n";
+    fs.outputFileSync(perProjectConfigFile, perProjectConfig);
+  }  else {
+    throw new Error('`config` directory does not exist.');
+  }
+}
